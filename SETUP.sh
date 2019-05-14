4b538c8dc3c180b9b52682069cf27f1b0f3bdc9a
僚介 <ryousuke@Kawashima-mba.local>
2019-05-14 10:15:31 +0900

    SETUP.sh modified

--- SETUP.sh
+++ SETUP.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo "**************************************************************"
 echo "This shellscript setups for APE_macro"
+curpath=`pwd`
 echo "**************************************************************"
 echo "This file write alias in ~/.bashrc"
 read -p "OK? [y/n]: " choice
 if [ "$choice" = "y" ]; then
 touch ~/.bashrc
 echo '# alias setting for APE macro' >>  ~/.bashrc
-echo 'alias maketmp="source /Users/ryousuke/GitHub/APE_macro/temp.sh"' >>  ~/.bashrc
-echo 'alias epp_temp="echo /Users/ryousuke/GitHub/APE_macro/"' >>  ~/.bashrc
+echo 'alias maketmp="source '$curpath'/temp.sh"' >>  ~/.bashrc
+echo 'alias epp_temp="echo '$curpath'/"' >>  ~/.bashrc
 echo '' >>  ~/.bashrc
+source ~/.bashrc
 echo "**************************************************************"
 echo "FINIFHED !"
 echo "**************************************************************"
